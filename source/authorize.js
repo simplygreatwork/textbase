1a40ce75f9190bca89b16aaef9e98b53e6688245
Philip Weaver <simplygreatwork@gmail.com>
2021-05-08 05:01:03 -0500

    shove

---
+++ source/authorize.js
@@ -0,0 +1,38 @@
+
+import { Logger } from '.source/logger.js'
+
+const logger = Logger()
+
+export function can(key) {
+	
+	let data = {}
+	bus.emit(key, data)
+	return data.result
+}
+
+function roadmap(selection) {
+	
+	let context = selection
+	can('insert-character', context)
+	can('insert-string', context)
+	can('insert-atom', context)
+	can('insert-atom:mention', context)
+	can('insert-card', context)
+	can('insert-card:code', context)
+	can('delete-character', context)
+	can('delete-block', context)
+	can('delete-content', context)
+	can('delete-atom', context)
+	can('delete-atom:mention', context)
+	can('delete-card', context)
+	can('delete-card:code', context)
+	can('format:underline', context)
+	can('toggle-block:h2', context)
+	can('cut', context)
+	can('copy', context)
+	can('paste', context)
+	can('paste:text/html', context)
+	can('paste:text/plain', context)
+	can('undo', context)
+	can('redo', context)
+}
