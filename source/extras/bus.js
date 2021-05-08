688d67e91e11c1930db174af7ecad4e3e03c2eef
Philip Weaver <simplygreatwork@gmail.com>
2021-05-08 14:40:52 -0500

    shove

--- source/extras/bus.js
+++ source/extras/bus.js
@@ -10,21 +10,23 @@ export function dump(bus) {
 	Object.keys(channels).forEach(function(context) {
 		Object.keys(channels[context]).forEach(function(key) {
 			index[key] = index[key] || {}
-			index[key][context] = index[key][context] || 0
-			index[key][context]++
+			index[key][context] = index[key][context] || channels[context][key]
 		})
 	})
 	let count = 0
 	let sorted = Array.from(Object.keys(index))
 	sorted.sort()
-	sorted.forEach(function(key, i) {
+	sorted.forEach(function(key) {
 		count++
 		let size = 0
 		let array = []
 		array.push(`${key}`)
 		Object.keys(index[key]).forEach(function(context) {
-			array.push(`(${context}:${index[key][context]})`)
-			size = size + index[key][context]
+			array.push(`(${context}:${index[key][context].length})`)
+			size = size + index[key][context].length
+			index[key][context].forEach(function(fn) {
+				if (false) console.log(`source: ${fn}`)
+			})
 		})
 		array.push(`(total:${size})`)
 		console.log(array.join(' '))
