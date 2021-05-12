c259cf0de46fc794edacd26912f704819ec26c46
Philip Weaver <simplygreatwork@gmail.com>
2021-05-11 22:32:13 -0500

    shove

--- source/basics.js
+++ source/basics.js
@@ -202,22 +202,6 @@ export function inject_script(bus, basis) {
 	document.body.appendChild(script)
 }
 
-export function resources_did_load(bus, fn) {
-	
-	let loading = new Set()
-	bus.on('resource-will-load', function(resource) {
-		logger('resources').log(`resource-will-load:${resource}`)
-		loading.add(resource)
-	})
-	bus.on('resource-did-load', function(resource) {
-		invoke_later(function() {														// ensure that the loading set will queue up instead of deflating too soon
-			logger('resources').log(`resource-did-load:${resource}`)
-			loading.delete(resource)
-			if (loading.size === 0) fn()
-		})
-	})
-}
-
 export function load_resources(fn, then) {
 	
 	let bus = new Bus()
